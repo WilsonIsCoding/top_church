ee63ee00a474330b4af9d789a108a5e3f17605c9
wilson <wilsoncheng@igs.com.tw>
2026-08-21 11:51:29 +0800

    修改

--- src/pages/submit.tsx
+++ src/pages/submit.tsx
@@ -216,7 +216,7 @@ export default function SubmitForm() {
 
       <div className="mb-6">
         <label className="block text-base font-semibold mb-2">
-          已完成第五梯次的{showAllMode ? "學員" : "組員"}：
+          已完成第六屆的{showAllMode ? "學員" : "組員"}：
         </label>
         <div className="max-h-96 overflow-y-auto border border-gray-300 rounded-lg bg-white shadow-inner py-2">
           {names.length === 0 ? (
